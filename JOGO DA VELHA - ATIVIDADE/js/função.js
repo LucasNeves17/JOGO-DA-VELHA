277ccd3826dc3ae58f74bf32e33c3873cb584ae2
LucasNeves17 <lucasneves_01@hotmail.com>
2019-06-13 22:17:09 -0300

    quarto commit - declaração de variaveis JS

---
+++ JOGO DA VELHA - ATIVIDADE/js/função.js
@@ -0,0 +1,9 @@
+const jogador1 = "X";
+const jogador2 = "O";
+var imagens = new Array();
+var primeiraJogada = jogador1;
+var fimdejogo = false;
+
+var rodadaAtual = 0;
+var fimdejogo = false;
+var sequencia = [];
