4c25c3a53d818579743848b11fe4123cdfd49cf0
LucasNeves17 <lucasneves_01@hotmail.com>
2019-06-14 00:26:16 -0300

    sexto commit - desenvolvendo JS

--- JOGO DA VELHA - ATIVIDADE/js/função.js
+++ JOGO DA VELHA - ATIVIDADE/js/função.js
@@ -8,3 +8,29 @@ var sequencia = [];
 var rodadaAtual = 0;
 var sequenciaPosicao = 0;
 var fimdeJogo = false;
+
+preloadImages("imagens/x.png","imagens/o.png")
+atualizaIndicador();
+inicializarEspacos();
+
+function preloadImages(){
+	for (i = 0; i < preloadImages.arguments.length; i++) {
+					images[i] = new Image()
+					images[i].src = preloadImages.arguments[i]
+				}
+}
+
+function atualizaIndicador(){
+
+	if (fimdeJogo) { return;}
+
+	if (primeiraJogada == jogador1) {
+
+		var player = document.querySelectorAll("div#indicador img")[0];
+		player.setAttribute("src", images[0].src);
+	} else{
+
+		var player = document.querySelectorAll("div#indicador img")[0];
+		player.setAttribute("src", images[1].src);
+	}
+}
